4ca9818a44da1014a55db694c5ce7259cd2f26ef
PACFWL <pablowholock@gmail.com>
2023-12-07 11:17:11 -0300

    music04

--- app/src/main/java/com/example/legato/objects/Music.java
+++ app/src/main/java/com/example/legato/objects/Music.java
@@ -1,22 +1,26 @@
 package com.example.legato.objects;
 
+import java.text.DateFormat;
+
 public class Music {
     private String id_music;
     private String nome_music;
-    private int ano_lancam_music;
+    private DateFormat ano_lancam_music;
     private String genero_music;
     private String album_music;
+    private String compositor_music;
 
     public Music() {
 
     }
 
-    public Music(String id_music, String nome_music, int ano_lancam_music, String genero_music, String album_music) {
+    public Music(String id_music, String nome_music, DateFormat ano_lancam_music, String genero_music, String album_music, String compositor_music) {
         this.id_music = id_music;
         this.nome_music = nome_music;
         this.ano_lancam_music = ano_lancam_music;
         this.genero_music = genero_music;
         this.album_music = album_music;
+        this.compositor_music = compositor_music;
     }
 
     public String getId_music() {
@@ -35,11 +39,11 @@ public class Music {
         this.nome_music = nome_music;
     }
 
-    public int getAno_lancam_music() {
+    public DateFormat getAno_lancam_music() {
         return ano_lancam_music;
     }
 
-    public void setAno_lancam_music(int ano_lancam_music) {
+    public void setAno_lancam_music(DateFormat ano_lancam_music) {
         this.ano_lancam_music = ano_lancam_music;
     }
 
@@ -57,5 +61,13 @@ public class Music {
 
     public void setAlbum_music(String album_music) {
         this.album_music = album_music;
+    }
+
+    public String getCompositor_music() {
+        return compositor_music;
+    }
+
+    public void setCompositor_music(String compositor_music) {
+        this.compositor_music = compositor_music;
     }
 }
